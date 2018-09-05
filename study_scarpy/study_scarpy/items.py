efb07294a042e6136935921e739009e5dafbd7af
czy <649954910@qq.com>
2018-09-05 08:49:43 +0800

    # 无MySQL操作测试--第四章7-15

--- study_scarpy/study_scarpy/items.py
+++ study_scarpy/study_scarpy/items.py
@@ -67,16 +67,16 @@ class JobBoleArticleItem(scrapy.Item):
     )
     content = scrapy.Field()
 
+
+# 无测试--第四章7-15
     # def get_insert_sql(self):
     #     insert_sql = """
     #         insert into jobbole_article(title, url, create_date, fav_nums, front_image_url, front_image_path,
     #         praise_nums, comment_nums, tags, content)
     #         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE content=VALUES(fav_nums)
     #     """
-
     #     fron_image_url = ""
     #     # content = remove_tags(self["content"])
-
     #     if self["front_image_url"]:
     #         fron_image_url = self["front_image_url"][0]
     #     params = (self["title"], self["url"], self["create_date"], self["fav_nums"],
--- study_scarpy/study_scarpy/pipelines.py
+++ study_scarpy/study_scarpy/pipelines.py
@@ -6,7 +6,10 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import codecs
 import json
+import pymysql
+pymysql.install_as_MySQLdb()
 
+from twisted.enterprise import adbapi
 from scrapy.pipelines.images import ImagesPipeline
 
 class StudyScarpyPipeline(object):
@@ -22,7 +25,9 @@ class ArticleImagePipeline(ImagesPipeline):
             item["front_image_path"] = image_file_path
         return item
 
- #自定义json文件的导出
+
+# 无效
+# 自定义json文件的导出
 class JsonWithEncodingPipeline(object):
     def __init__(self):
         self.file = codecs.open('article.json', 'w', encoding="utf-8")
@@ -32,3 +37,55 @@ class JsonWithEncodingPipeline(object):
         return item
     def spider_closed(self, spider):
         self.file.close()
+
+
+# 无测试--第四章7-15
+class MysqlPipeline(object):
+    #采用同步的机制写入mysql
+    def __init__(self):
+        self.conn = pymysql.connect('192.168.0.106', 'root', 'root', 'article_spider', charset="utf8", use_unicode=True)
+        self.cursor = self.conn.cursor()
+
+    def process_item(self, item, spider):
+        insert_sql = """
+            insert into jobbole_article(title, url, create_date, fav_nums)
+            VALUES (%s, %s, %s, %s)
+        """
+        self.cursor.execute(insert_sql, (item["title"], item["url"], item["create_date"], item["fav_nums"]))
+        self.conn.commit()
+
+# 无测试--第四章7-15
+class MysqlTwistedPipline(object):
+    def __init__(self, dbpool):
+        self.dbpool = dbpool
+
+    @classmethod
+    def from_settings(cls, settings):
+        dbparms = dict(
+            host = settings["MYSQL_HOST"],
+            db = settings["MYSQL_DBNAME"],
+            user = settings["MYSQL_USER"],
+            passwd = settings["MYSQL_PASSWORD"],
+            charset='utf8',
+            cursorclass=pymysql.cursors.DictCursor,
+            use_unicode=True,
+        )
+        dbpool = adbapi.ConnectionPool("MySQLdb", **dbparms)
+
+        return cls(dbpool)
+
+    def process_item(self, item, spider):
+        #使用twisted将mysql插入变成异步执行
+        query = self.dbpool.runInteraction(self.do_insert, item)
+        query.addErrback(self.handle_error, item, spider) #处理异常
+
+    def handle_error(self, failure, item, spider):
+        # 处理异步插入的异常
+        print (failure)
+
+    def do_insert(self, cursor, item):
+        #执行具体的插入
+        #根据不同的item 构建不同的sql语句并插入到mysql中
+        insert_sql, params = item.get_insert_sql()
+        print (insert_sql, params)
+        cursor.execute(insert_sql, params)
